0b0fb1316f0718a7fd10ba9b11f7cc195044f829
Johan Sebastiaan Koetsier <skoetsier@meta.com>
2025-12-02 21:13:52 +0800

    Fix import errors in views.py
    - Fix import: MigrateVMForm -> MigrationForm
    - Add Q to imports from django.db.models
    - Remove duplicate Q import from task_list function

--- proxmox_manager/views.py
+++ proxmox_manager/views.py
@@ -1,12 +1,12 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
-from django.db.models import Avg
+from django.db.models import Avg, Q
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.utils import timezone
 
-from .forms import MigrateVMForm, SnapshotForm
+from .forms import MigrationForm, SnapshotForm, VMSearchForm
 from .models import AuditLog, CeleryTask, Node, ProxmoxCluster, VirtualMachine
 from .tasks import (
     create_snapshot,
@@ -538,8 +538,6 @@ def task_list(request):
     # Search by VM name or cluster name
     search = request.GET.get("search")
     if search:
-        from django.db.models import Q
-
         tasks = tasks.filter(
             Q(vm__name__icontains=search)
             | Q(cluster__name__icontains=search)
